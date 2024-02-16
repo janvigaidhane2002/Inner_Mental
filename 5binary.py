67a5692ebe7071d15592cd6395bd13458d865869
Shrushti Ganorkar <119885951+ganorkarshrushti@users.noreply.github.com>
2024-02-16 19:40:51 +0530

    Add files via upload

---
+++ 5binary.py
@@ -0,0 +1,27 @@
+
+def BinarySearch(list1,start,end,num):
+    mid = (start + end)//2
+    # for i in range(0,len(list1)):
+    if(num == list1[mid]):
+            return mid
+    elif(num > list1[mid]):
+        start = mid + 1
+        return BinarySearch(list1,mid+1,end,num)
+    elif(num < list1[mid]):
+        end = mid - 1
+        return BinarySearch(list1,start,mid-1,num)
+    else:
+          return -1
+    
+
+def main():
+    list1= [1,2,3,4,5]
+    num = int(input("Enter the number to be found: "))
+    res = BinarySearch(list1,0,len(list1),num)
+    if(res == -1):
+            print("The element is not found")
+    else:
+            print(f"The number is found at {res}")    
+
+if __name__ == "__main__":
+    main()
